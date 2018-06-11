e9314f8a9f4324b631246dcdf05992f66727fb82
Gogs2 <gjorgji.chepujnoski@gmail.com>
2018-06-11 20:06:38 +0200

    TESTING

---
+++ 2048/2048/Square.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace _2048
+{
+    class Square
+    {
+        Point pos;
+        Color color;
+        int value;
+        // Site kvadratcinja ke se so 50px strana
+        public static int side = 50;
+        public Square(Point pos, int value, Color color)
+        {
+            this.pos = pos;
+            this.value = value;
+            this.color = color;
+        }
+        public void Draw(Graphics g)
+        {
+            g.FillRectangle
+        }
+    }
+}
